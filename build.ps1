2fcf398d7bd6424d139da0dbb330dc31d887d33c
Vlad Aleksandrov <vladimir.aleksandrov@checkout.com>
2018-04-19 15:20:57 +0200

    Revert changes

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-$version='9.11-slim-ci'
+$version='9.11-slim'
 docker build -t vladimiraleksandrovcko/docker-chakram-nightmare:$version .
 docker push vladimiraleksandrovcko/docker-chakram-nightmare:$version
